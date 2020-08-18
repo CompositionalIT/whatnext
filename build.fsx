8182c18569e19bc3f043ded049f491b788c28a20
Isaac Abraham <isaac.abraham@gmail.com>
2020-08-18 18:57:10 +0200

    Incorporate deploy

--- build.fsx
+++ build.fsx
@@ -49,16 +49,42 @@ Target.create "Bundle" (fun _ ->
 )
 
 Target.create "Azure" (fun _ ->
-    let web = webApp {
-        name "whatnext"
-        zip_deploy "deploy"
-    }
-    let deployment = arm {
-        location Location.WestEurope
-        add_resource web
+    let storage = storageAccount {
+        name "whatnextfsstore"
     }
 
-    deployment
+    let cosmos = cosmosDb {
+        account_name "citstore"
+        name "whatnextdb"
+        free_tier
+        add_containers [
+            cosmosContainer {
+                name "projects"
+                partition_key [ "name" ] CosmosDb.IndexKind.Hash
+            }
+        ]
+    }
+
+    // Piggy back on an existing service plan in another resource group...
+    let servicePlan = CoreTypes.ArmExpression.resourceId(Arm.Web.serverFarms, ResourceName "Default1", "Internal-CIT")
+
+    let app = webApp {
+        name "whatnextfs"
+        link_to_unmanaged_service_plan (servicePlan.Eval())
+        setting "storage_key" storage.Key
+        setting "cosmos_key" cosmos.PrimaryConnectionString
+        app_insights_off
+        zip_deploy "deploy"
+        depends_on storage
+        depends_on cosmos
+    }
+
+    let template = arm {
+        location Location.NorthEurope
+        add_resources [ storage; cosmos; app ]
+    }
+
+    template
     |> Deploy.execute "whatnext" Deploy.NoParameters
     |> ignore
 )
